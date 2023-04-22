11bb88b21677e0ab4bafa6b95eda4cd8272e5c51
SOURCEHAMD1 <131338472+SOURCEHAMD1@users.noreply.github.com>
2023-04-22 17:50:39 +0300

    Update help.py

--- AnonX/plugins/help.py
+++ AnonX/plugins/help.py
@@ -23,7 +23,9 @@ HELP_COMMAND = get_command("HELP_COMMAND")
 @app.on_message(
     filters.command(HELP_COMMAND) & filters.private & ~filters.edited & ~BANNED_USERS
 )
-@app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
+@app.on_callback_query(
+    filters.regex("settings_back_helper") & ~BANNED_USERS
+)
 async def helper_private(
     client: app, update: Union[types.Message, types.CallbackQuery]
 ):
@@ -38,10 +40,13 @@ async def helper_private(
         _ = get_string(language)
         keyboard = help_pannel(_, True)
         if update.message.photo:
-            await update.message.delete()
-            await update.message.reply_text(_["help_1"], reply_markup=keyboard)
+            await update.edit_message_text(
+                _["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
+            )
         else:
-            await update.edit_message_text(_["help_1"], reply_markup=keyboard)
+            await update.edit_message_text(
+                _["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
+            )
     else:
         chat_id = update.chat.id
         if await is_commanddelete_on(update.chat.id):
@@ -52,7 +57,10 @@ async def helper_private(
         language = await get_lang(chat_id)
         _ = get_string(language)
         keyboard = help_pannel(_)
-        await update.reply_text(_["help_1"], reply_markup=keyboard)
+        await update.reply_sticker("CAACAgUAAxkBAAIjVmKPYTFByKZlCo9d8mUv8QVAJEw7AAL9BQACiy14VGoQxOCDfE1KJAQ")
+        await update.reply_photo(
+            photo=config.START_IMG_URL,
+            caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard)
 
 
 @app.on_message(
