e2233af0341ed687318e59b273de55088ca8f016
SOURCEHAMD1 <131338472+SOURCEHAMD1@users.noreply.github.com>
2023-04-22 17:32:19 +0300

    Update help.py

--- AnonX/utils/inline/help.py
+++ AnonX/utils/inline/help.py
@@ -59,7 +59,7 @@ def help_back_markup(_):
             [
                 InlineKeyboardButton(
                     text=_["BACK_BUTTON"],
-                    callback_data=f"settingsback_helper",
+                    callback_data=f"settings_back_helper",
                 ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"], callback_data=f"إغلاق"
