dd2a0575fd47e06e738c61b9a2908d383b6e2fa4
SOURCEHAMD1 <131338472+SOURCEHAMD1@users.noreply.github.com>
2023-04-24 02:11:38 +0300

    Update bot.py

--- الصانع/bot.py
+++ الصانع/bot.py
@@ -6,16 +6,16 @@ from pyromod import listen
 
 bot = Client(
     "mo",
-    api_id=8186557,
-    api_hash="efd77b34c69c164ce158037ff5a0d117",
-    bot_token="5912908363:AAGkAqH3VTR0d-g0lAervmElqo9TqgDYLu4",#توكن المصنع
-    plugins=dict(root="MHelal")
+    api_id=21468057,
+    api_hash="f4a868976632fec2260eb7a7f9d88720",
+    bot_token="6285659665:AAGZvJZmLPpPjFYav6W31poajqz-Sc-mAMw",#توكن المصنع
+    plugins=dict(root="ah_2_v")
     )
 
 async def start_helalbot():
     print("تم تشغيل الصانع بنجاح..💗")
     await bot.start()
-    hh = "MohamedHelal_l"#يوزر المطور المصنع
+    hh = "ah_2_v"#يوزر المطور المصنع
     try:
         await bot.send_message(hh, "**تم تشغيل الصانع بنجاح ...🥀**")
     except:
