136588c3a0b5cd894a4cb154911a1ab0017d6aca
SOURCEHAMD1 <131338472+SOURCEHAMD1@users.noreply.github.com>
2023-06-15 20:20:11 +0300

    المطور.py

--- AnonX/plugins/المطور.py
+++ AnonX/plugins/المطور.py
@@ -21,7 +21,35 @@ from random import  choice, randint
                 
                 
 @app.on_message(
-    command(["المطور","مبرمج","مطور السورس","المبرمج"])
+    command(["المطور","مطور"])
+    & ~filters.edited
+)
+async def huhh(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/550a336110068bf81b891.jpg",
+        caption=f"""**★⊷⧼𝑠𝑜𝑢𝑟𝑐𝑒 𝑎𝑟𝑛𝑜𝑝 ‌ㇱ⧽⊶★**\nمرحبا بك عزيزي {message.from_user.mention} في قسم مطور سورس ميوزك\nللتحدث مع مطور السورس اضغط علي الازرار بالاسفل👇\n**★⊷⧼𝑠𝑜𝑢𝑟𝑐𝑒 𝑎𝑟𝑛𝑜𝑝 ‌ㇱ⧽⊶★**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                text="مطور البوت 👨‍💻", user_id=OWNER 
+                 ],[
+                   InlineKeyboardButton(
+                        "𝑠𝑜𝑢𝑟𝑐𝑒 𝑎𝑟𝑛𝑜𝑝 ‌ㇱ", url=f"https://t.me/N_G_122"),
+                ],
+
+            ]
+
+        ),
+
+    )
+
+
+
+
+
+@app.on_message(
+    command(["مبرمج","المبرمج"])
     & ~filters.edited
 )
 async def huhh(client: Client, message: Message):
@@ -43,10 +71,6 @@ async def huhh(client: Client, message: Message):
         ),
 
     )
-
-
-
-
 
 
 
