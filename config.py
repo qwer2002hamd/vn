7e125933034b0e5fc5efc241dc5c21d35c4fbc91
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-06-19 22:57:38 +0300

    config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/SOURCEHAMD1/arnop")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/qwer2002hamd/vn")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
