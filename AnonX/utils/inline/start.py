6470ed1fbebd7eb6eaa8df4d1c918f2f74123769
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-06-24 00:40:28 +0300

    start.py

---
+++ AnonX/utils/inline/start.py
@@ -0,0 +1,57 @@
+from typing import Union
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+import config
+
+
+
+def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="اضغط لاضافه البوت لمجموعتك",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="‹ اوامر التشغيل ›",
+                callback_data="settings_back_helper",
+            ),
+            InlineKeyboardButton(
+                text="‹ الاعدادات ›", callback_data="settings_helper"
+            ),
+        ],
+     ]
+    return buttons
+
+
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="اضغط لاضافه البوت لمجموعتك",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="‹ طريقه التفعيل ›", callback_data="help_callback hb6"
+            ), 
+            InlineKeyboardButton(
+                text="‹ اوامر التشغيل ›", callback_data="settings_back_helper"
+             )
+        ],
+        [
+            InlineKeyboardButton(
+                text="‹ المطور ›", user_id=OWNER
+            )
+        ],
+        [ 
+            InlineKeyboardButton(
+                text="‹ قـناة الـسورس ›", url=f"https://t.me/ah07v"
+            )
+        ],
+     ]
+    return buttons
