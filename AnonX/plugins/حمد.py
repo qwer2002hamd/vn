085e12e030869da8ce5b68de88b3992c01424298
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-06-25 02:53:48 +0300

    حمد.py

--- AnonX/plugins/حمد.py
+++ AnonX/plugins/حمد.py
@@ -35,7 +35,7 @@ REPLY_MESSAGE_BUTTONS = [
 ]
 
   
-@app.on_message(filters.private & command(["/start","/HAMD"]))
+@app.on_message(filters.private & command("/HAMD"))
 async def cpanel(_, message: Message):             
         text = REPLY_MESSAGE
         reply_markup = ReplyKeyboardMarkup(REPLY_MESSAGE_BUTTONS, resize_keyboard=True)
