7b30db875ccc7b7fb74275ae12c044eea029171c
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-06-24 16:53:03 +0300

    help.py

--- AnonX/plugins/help.py
+++ AnonX/plugins/help.py
@@ -92,3 +92,5 @@ async def helper_cb(client, CallbackQuery, _):
         await CallbackQuery.edit_message_text(helpers.HELP_4, reply_markup=keyboard)
     elif cb == "hb5":
         await CallbackQuery.edit_message_text(helpers.HELP_5, reply_markup=keyboard)
+    elif cb == "hb6":
+        await CallbackQuery.edit_message_text(helpers.HELP_6, reply_markup=keyboard)
