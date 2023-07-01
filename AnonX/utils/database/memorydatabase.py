6e68718e5d3e62399b0edebf8c19d698e36b3c6d
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-01 21:18:02 +0300

    memorydatabase.py

--- AnonX/utils/database/memorydatabase.py
+++ AnonX/utils/database/memorydatabase.py
@@ -11,7 +11,7 @@
 
 import config
 from config import PRIVATE_BOT_MODE
-from AlexaMusic.core.mongo import mongodb
+from AnonX.core.mongo import mongodb
 
 channeldb = mongodb.cplaymode
 commanddb = mongodb.commands
@@ -20,7 +20,7 @@ playmodedb = mongodb.playmode
 playtypedb = mongodb.playtypedb
 langdb = mongodb.language
 authdb = mongodb.adminauth
-videodb = mongodb.yukkivideocalls
+videodb = mongodb.anonvideocalls
 onoffdb = mongodb.onoffper
 suggdb = mongodb.suggestion
 autoenddb = mongodb.autoend
