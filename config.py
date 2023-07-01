4160fca3e80b99022ea740d44e8dd435d2ee8952
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-01 22:24:46 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,148 @@
+import re
+import sys
+from os import getenv
+
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+BOT_TOKEN = getenv("BOT_TOKEN")
+
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "˹ʜᴀᴍᴅ ✘ ᴍᴜsɪᴄ˼ 🫧🕊️⃝")
+
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1748768168").split()))
+
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
+
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/qwer2002hamd/vn")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+GIT_TOKEN = getenv("GIT_TOKEN", None)
+
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ah07v")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/ah07v")
+
+SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
+
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "180"))
+SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "180"))
+
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
+AUTO_LEAVE_ASSISTANT_TIME = int(
+    getenv("ASSISTANT_LEAVE_TIME", "5400")
+)
+
+AUTO_DOWNLOADS_CLEAR = getenv("AUTO_DOWNLOADS_CLEAR", "True")
+
+PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
+
+YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", "5"))
+TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "3"))
+
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
+
+VIDEO_STREAM_LIMIT = int(getenv("VIDEO_STREAM_LIMIT", "5"))
+SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "50"))
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "50"))
+
+CLEANMODE_DELETE_MINS = int(getenv("CLEANMODE_MINS", "12"))
+
+TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", "104857600"))
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "1073741824"))
+# https://www.gbmb.org/mb-to-bytes
+
+STRING1 = getenv("STRING_SESSION", None)
+STRING2 = getenv("STRING_SESSION2", None)
+STRING3 = getenv("STRING_SESSION3", None)
+STRING4 = getenv("STRING_SESSION4", None)
+STRING5 = getenv("STRING_SESSION5", None)
+
+BANNED_USERS = filters.user()
+YTDOWNLOADER = 1
+LOG = 2
+LOG_FILE_NAME = "logs.txt"
+adminlist = {}
+lyrical = {}
+chatstats = {}
+userstats = {}
+clean = {}
+autoclean = []
+
+
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/a0b4ae6f5304825e45f8e.jpg")
+
+PING_IMG_URL = getenv(
+    "PING_IMG_URL",
+    "https://telegra.ph/file/4d4eca0ac2202c09cd2e8.jpg",
+)
+
+PLAYLIST_IMG_URL = "https://te.legra.ph/file/660cacb5b70552494326d.jpg"
+
+GLOBAL_IMG_URL = "https://te.legra.ph/file/f3c543289d3e168058089.jpg"
+
+STATS_IMG_URL = "https://te.legra.ph/file/b9a6d1b97b5f0fd2d6ed6.jpg"
+
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/127046ebd6e8a2392e543.jpg"
+
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/7d1352685054234c22c54.jpg"
+
+STREAM_IMG_URL = "https://te.legra.ph/file/5297a508644d7231069e4.jpg"
+
+SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/a9d48a1666b8b04f2bca2.jpg"
+
+YOUTUBE_IMG_URL = getenv(
+    "YOUTUBE_IMG_URL",
+    "assets/Youtube.jpeg"
+
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/42d626435044551dc09bd.jpg"
+
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/9661b72a271071ebee85e.jpg"
+
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/7bfaa569ad7f7a4561acc.jpg"
+
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(
+        int(x) * 60**i
+        for i, x in enumerate(reversed(stringt.split(":")))
+    )
+
+
+DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
+SONG_DOWNLOAD_DURATION_LIMIT = int(
+    time_to_seconds(f"{SONG_DOWNLOAD_DURATION}:00")
+)
+
+
+if UPSTREAM_REPO:
+    if not re.match("(?:http|https)://", UPSTREAM_REPO):
+        print(
+            "[ERROR] - Your UPSTREAM_REPO url is wrong. Please ensure that it starts with https://"
+        )
+        sys.exit()
+
+ if YOUTUBE_IMG_URL:
+    if YOUTUBE_IMG_URL != "assets/Youtube.jpeg":
+        if not re.match("(?:http|https)://", YOUTUBE_IMG_URL):
+            print(
+                "[ERROR] - Your YOUTUBE_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+            sys.exit()      
+
+if PING_IMG_URL:
+    if PING_IMG_URL != "assets/Ping.jpeg":
+        if not re.match("(?:http|https)://", PING_IMG_URL):
+            PING_IMG_URL = "https://telegra.ph/file/4d4eca0ac2202c09cd2e8.jpg"
+
+if START_IMG_URL:
+    if START_IMG_URL != "assets/Ping.jpeg":
+        if not re.match("(?:http|https)://", START_IMG_URL):
+            START_IMG_URL = "https://telegra.ph/file/a0b4ae6f5304825e45f8e.jpg"
