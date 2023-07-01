da25549b4222471d57a5f9dfdb7f633d60978fbb
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-01 18:41:30 +0300

    unmute.py

--- AnonX/plugins/unmute.py
+++ AnonX/plugins/unmute.py
@@ -5,7 +5,6 @@ from config import BANNED_USERS
 from strings import get_command
 from AnonX import app
 from AnonX.core.call import Anon
-from AnonX.utils.database import is_muted, mute_off
 from AnonX.utils.decorators import AdminRightsCheck
 
 # Commands
