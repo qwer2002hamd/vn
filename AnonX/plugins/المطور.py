3a658604b963c1d762392e587af8d04cb6aa9285
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-07 21:24:33 +0300

    Update المطور.py

--- AnonX/plugins/المطور.py
+++ AnonX/plugins/المطور.py
@@ -40,4 +40,22 @@ async def huhh(client: Client, message: Message):
 
 
 
-    
+@app.on_message(
+    command(["المطور"])
+    & filters.group
+    & ~filters.edited
+)
+async def yas(client, message):
+    usr = await client.get_chat("MGJ_D")
+    name = usr.first_name
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"🧞‍♂️ ¦𝙽𝙰𝙼𝙴 :{name}\n🎯 ¦𝚄𝚂𝙴𝚁 :@{usr.username}\n💣 ¦𝙸𝙳 :`{usr.id}`\n🚀 ¦𝙱𝙸𝙾 :{usr.bio}", 
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{usr.username}")
+                ],
+            ]
+        ),
+    )    
