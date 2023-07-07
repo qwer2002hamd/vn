fcdb23a82836e01775b5f779ea2a842f5ce7a120
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-07 21:39:46 +0300

    Update المطور.py

--- AnonX/plugins/المطور.py
+++ AnonX/plugins/المطور.py
@@ -14,39 +14,14 @@ from AnonX import app
 from random import  choice, randint
 
                 
+
+
 @app.on_message(
     command(["مبرمج","المبرمج","المبرمـج"])
     & ~filters.edited
 )
-async def huhh(client: Client, message: Message):
-    await message.reply_photo(
-        photo=f"https://telegra.ph/file/a0b4ae6f5304825e45f8e.jpg",
-        caption=f"""مرحبا بك عزيزي {message.from_user.mention} في قسم مطور سورس ميوزك\nللتحدث مع مطور السورس اضغط علي الازرار بالاسفل👇""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "𝑌.𝑂²¹ ͢͢➛ℍ𝗺!ِٰ𝗱♪", url=f"https://t.me/ah_2_v"), 
-                 ],[
-                   InlineKeyboardButton(
-                        "‹ قـناة الـسورس ›", url=f"https://t.me/ah07v"),
-                ],
-
-            ]
-
-        ),
-
-    )
-
-
-
-@app.on_message(
-    command(["المطور"])
-    & filters.group
-    & ~filters.edited
-)
 async def yas(client, message):
-    usr = await client.get_chat("MGJ_D")
+    usr = await client.get_chat("ah_2_v")
     name = usr.first_name
     photo = await app.download_media(usr.photo.big_file_id)
     await message.reply_photo(photo,       caption=f"🧞‍♂️ ¦𝙽𝙰𝙼𝙴 :{name}\n🎯 ¦𝚄𝚂𝙴𝚁 :@{usr.username}\n💣 ¦𝙸𝙳 :`{usr.id}`\n🚀 ¦𝙱𝙸𝙾 :{usr.bio}", 
